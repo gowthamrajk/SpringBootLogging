504a7203f8453164baddbf421a5684250fc59ffc
GowthamRaj.K <43011442+gowthamrajk@users.noreply.github.com>
2021-05-30 20:38:24 +0530

    Add files via upload

---
+++ SpringBootLogging/src/main/java/com/application/SpringBootLoggingApplication.java
@@ -0,0 +1,37 @@
+package com.application;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootLoggingApplication 
+{
+
+	Logger log = LoggerFactory.getLogger(SpringBootLoggingApplication.class);
+
+	@GetMapping("/test/{name}")
+	public String greeting(@PathVariable String name)
+	{
+		log.debug("Request {}", name);
+		
+		if (name.equalsIgnoreCase("test")) 
+		{
+			throw new RuntimeException("Opps Exception raised....");
+		}
+		
+		String response = "Hello! " + name + ", Welcome to Logging application by GowthamRaj";
+		log.debug("Response {}", response);
+		return response;
+	}
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(SpringBootLoggingApplication.class, args);
+	}
+}
